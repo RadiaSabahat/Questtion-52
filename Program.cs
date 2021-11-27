8f73395af1c75d2fe8db3998bc2f4f2d271a640b
radia sabahat <radiasabahat@gmail.com>
2021-11-27 09:54:52 -0800

    firt commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine(1 + 8 / 2 + ((1 * 4) + (5 * 4)) / 4);
+Console.WriteLine((1 + 1 + 1 + 1) / 2 + (1 + 1 + 1) / 3);
+Console.WriteLine(5 * 5 + 5 / 5 + 6);
+Console.WriteLine(((3 + 4) + (4 * 7)) / 5);
+Console.WriteLine((3 * 6 * 7 * 2) + 12 / 2);
+Console.WriteLine(5 - 3 * 4 % (6 - 1));
+Console.WriteLine((8 * 4 * 2 + 6) / 2 + 4);
